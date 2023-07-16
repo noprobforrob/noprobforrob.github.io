d65f0bdf9bcd971f9c0b9b967566f7327d17bbf6
robin <robin.gludovatz@gmail.com>
2023-07-16 22:28:07 +0200

    Tik Tak Toe js

---
+++ m3/footer.js
@@ -0,0 +1,10 @@
+document.write(
+    '<footer class="text-center bg-secondary bg-opacity-50 p-5">\n' +
+    '    <a class="up-arrow" href="#top" title="nach oben">\n' +
+    '        <i class="fa fa-chevron-up" aria-hidden="true"></i>\n' +
+    '    </a>\n' +
+    '    <p class="text-center">\n' +
+    '        &copy; 2023 <a href="https://mmf.univie.ac.at/" target="_blank">Projekt MmF</a> | <a href="impressum.html" target="_blank">Impressum</a> | <a href="datenschutz.html" target="_blank">Datenschutz</a> | <a href="sponsoren.html">Sponsoren</a>\n' +
+    '    </p>\n' +
+    '</footer>'
+);
---
+++ m3/navigation.js
@@ -0,0 +1,25 @@
+document.write('<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">\n' +
+    '            <span class="navbar-toggler-icon"></span>\n' +
+    '        </button>\n' +
+    '        <div class="offcanvas offcanvas-end justify-content-lg-center px-3" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="background-color: rgb(40,47,105);">\n' +
+    '            <div class="offcanvas-header">\n' +
+    '                <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>\n' +
+    '                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>\n' +
+    '            </div>\n' +
+    '            <div class="offcanvas-body justify-content-lg-center justify-content-end h5">\n' +
+    '                <ul class="navbar-nav p-3">\n' +
+    '                    <li class="nav-item mx-2">\n' +
+    '                        <a class="nav-link" id="wettbewerb" aria-current="page" href="index.html">Wettbewerb</a>\n' +
+    '                    </li>\n' +
+    '                    <li class="nav-item mx-2">\n' +
+    '                        <a class="nav-link" id="preise" aria-current="page" href="preise.html">Preise</a>\n' +
+    '                    </li>\n' +
+    '                    <li class="nav-item mx-2">\n' +
+    '                        <a class="nav-link" id="vorbereitung" aria-current="page" href="vorbereitung.html">Vorbereitung</a>\n' +
+    '                    </li>\n' +
+    '                    <li class="nav-item mx-2">\n' +
+    '                        <a class="nav-link" id="kontakt" aria-current="page" href="kontakt.html">Kontakt</a>\n' +
+    '                    </li>\n' +
+    '                </ul>\n' +
+    '            </div>\n' +
+    '        </div>');
